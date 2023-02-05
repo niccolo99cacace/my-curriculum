1413dc3feed375c9b03cf0d87d9017b9343f914d
niccolo99cacace <niccolo99cacace@gmail.com>
2023-02-05 19:21:09 +0100

    Updates

---
+++ precache-manifest.bc08ef80a4af92490cb846d76e166d23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8711b98036a1d0e6cd8",
+    "url": "/my-curriculum/static/css/main.fbeb023f.chunk.css"
+  },
+  {
+    "revision": "b8711b98036a1d0e6cd8",
+    "url": "/my-curriculum/static/js/main.b8711b98.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/my-curriculum/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/my-curriculum/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "f6be653c1b41ee7ed7bd",
+    "url": "/my-curriculum/static/js/runtime~main.f6be653c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-curriculum/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5c0ea532c0ce9b124614118871e41783",
+    "url": "/my-curriculum/index.html"
+  }
+];
